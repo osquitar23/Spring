adce6b49fb3dae4b097a2a301792d714ea9b4d3b
Curso mañana <Curso maana@PC04a5>
2018-01-30 12:48:24 +0100

    Springggg Welcome To The Night

--- springapp/src/main/java/com/companyname/springapp/web/InventoryController.java
+++ springapp/src/main/java/com/companyname/springapp/web/InventoryController.java
@@ -4,6 +4,8 @@ package com.companyname.springapp.web;
 /*Aqui se importan las librerias o las clases*/
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,15 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.companyname.springapp.service.ProductManager;
+
 @Controller/*Corresponde a la libreria de spring*/
-public class HelloController {
+public class InventoryController {
 
     protected final Log logger = LogFactory.getLog(getClass());
     /*Mapeos o redireccion*/
+    
+    @Autowired
+    private ProductManager productManager;
+
+    
     @RequestMapping(value="/hello.htm")/*request mapping me hace un mapeo del jsp al htm 
     o me hace una conversion de fichero*/
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
@@ -29,8 +39,13 @@ public class HelloController {
         logger.info("Returning hello view with " + now);/*Variable de la fecha*//*libreria para sacar los mensajes
         en consola->Logger, retorna un modelandview con el info.*/
         
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
+
+        
         /*El crea una variable cualquiera*/
-        return new ModelAndView("hello", "now", now);/*y esto hace que me devuelva el jsp y la fecha
+        return new ModelAndView("hello", "model", myModel);/*y esto hace que me devuelva el jsp y la fecha
         del servidor*//*hello.jsp es la vista y el primer now es lo que va a mostrarme en el jsp y el otro now es 
         que hace que me cargue la variable con el framework creado*/
     }
